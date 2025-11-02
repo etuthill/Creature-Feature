02764d5c3c5a5b8caa54f90b7127e2ddb82602ea
etuthill <etuthillolin@gmail.com>
2025-11-02 18:05:42 -0500

    file organization

---
+++ sprint1/quick_servo_test/quick_servo_test.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+
+Servo motor1;
+Servo motor2;
+
+int minAngle = 0;
+int maxAngle = 90;
+int stepSize = 1;
+int delayTime = 30;    
+
+void setup() {
+  motor1.attach(13);
+  motor2.attach(12);
+}
+
+void loop() {
+  for (float pos = minAngle; pos <= maxAngle; pos += stepSize) {
+    motor1.write(pos);
+    motor2.write(maxAngle - pos);
+    delay(delayTime);
+  }
+
+  for (float pos = maxAngle; pos >= minAngle; pos -= stepSize) {
+    motor1.write(pos);
+    motor2.write(maxAngle - pos);
+    delay(delayTime);
+  }
+}
