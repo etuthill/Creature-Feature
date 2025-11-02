87335220d9faf62fb6a04266e28cabd6ea843c7c
bugsyflyer <103437604+bugsyflyer@users.noreply.github.com>
2025-11-02 16:35:25 -0500

    edited sprint_1
    changed "motor" to servo
    changed loop to be more smooth hopefully

--- sprint_1.ino
+++ sprint_1.ino
@@ -1,30 +1,30 @@
 #include <Servo.h>
 
-Servo motor1;
-Servo motor2;
+Servo servo1;
+Servo servo2;
 
 int minAngle = 0;
 int maxAngle = 90;
 int stepSize = 1;
-int delayTime = 30;    
+int delayTime = 1;    
 
 void setup() {
-  motor1.attach(13);
-  motor2.attach(12);
+  servo1.attach(13);
+  servo2.attach(12);
 }
 
  
 
 void loop() {
   for (float pos = minAngle; pos <= maxAngle; pos += stepSize) {
-    motor1.write(pos);
-    motor2.write(maxAngle - pos);
+    servo1.writeMicroseconds(pos);
+    servo2.writeMicroseconds(maxAngle - pos);
     delay(delayTime);
   }
 
   for (float pos = maxAngle; pos >= minAngle; pos -= stepSize) {
-    motor1.write(pos);
-    motor2.write(maxAngle - pos);
+    servo1.writeMicroseconds(pos);
+    servo2.writeMicroseconds(maxAngle - pos);
     delay(delayTime);
   }
 }
