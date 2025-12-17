7cf3d35d9b72d106da9429ce26ff1262b3ee0e6a
etuthill <etuthillolin@gmail.com>
2025-12-17 01:59:46 -0500

    undo sensitivt change

--- elias_playground/full_arduino/full_arduino.ino
+++ elias_playground/full_arduino/full_arduino.ino
@@ -25,8 +25,8 @@ const int forceSensor = A0;
 const int hallSensor = A1;
 
 //sensor vars
-int hallMin = 400;
-int hallMax = 600;
+int hallMin = 350;
+int hallMax = 650;
 int forceThreshold = 1000;
 
 // debounce interactions
