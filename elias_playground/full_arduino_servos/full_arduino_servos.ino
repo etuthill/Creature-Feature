5cdee815c6d8acfce93ac6a1cc5fe5bd19cd800d
etuthill <etuthillolin@gmail.com>
2025-12-18 02:29:33 -0500

    add extra math to sine

--- elias_playground/full_arduino_servos/full_arduino_servos.ino
+++ elias_playground/full_arduino_servos/full_arduino_servos.ino
@@ -396,9 +396,11 @@ void updateServoSine() {
   float t = elapsed / 1000.0;// seconds
   float omega = TWO_PI * sineFrequency;
 
-  int leftPos  = 90 - 60 * sin(omega * t);
-  int rightPos = 90 + 60 * sin(omega * t);
-  int backPos  = 90 + 60 * sin(omega * t);
+  float shift = (2*3.1415926535897932384626433832795028841971693993751058209749445923)/3;
+
+  float leftPos  = 90 - 60 * sin(omega * t - shift);
+  float rightPos = 90 + 60 * sin(omega * t + shift);
+  float backPos  = 90 + 60 * sin(omega * t);
 
   leftServo.write(constrain(leftPos,  30, 150));
   rightServo.write(constrain(rightPos, 30, 150));
