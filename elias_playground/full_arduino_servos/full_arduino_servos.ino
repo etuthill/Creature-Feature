d140a5db013d6b51ab173bf3cb748ecf8c1b6255
etuthill <etuthillolin@gmail.com>
2025-12-18 03:09:10 -0500

    go back to 0.5 Hz

--- elias_playground/full_arduino_servos/full_arduino_servos.ino
+++ elias_playground/full_arduino_servos/full_arduino_servos.ino
@@ -11,7 +11,7 @@ unsigned long eatExitTime = 0;
 bool eatCooldownArmed = false;
 
 // sine motion params
-const float sineFrequency = 0.33;
+const float sineFrequency = 0.5;
 const unsigned long sineDuration = 10000; // ms (5 seconds)
 
 unsigned long sineStartTime = 0;
